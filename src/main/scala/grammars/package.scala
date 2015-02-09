79f3656d82b17d4152999be49742343c0441e535
William O'Hanley <liam@wohanley.com>
2015-02-09 16:58:44 +1300

    this compiles wow

--- src/main/scala/grammars/package.scala
+++ src/main/scala/grammars/package.scala
@@ -1,21 +1,46 @@
 package object grammars {
 
-  /** Products are things that can be on the right-hand side of a grammar
-    * rule. */
-  trait Product
-  case class Symbol(name: String) extends Product
-  case class Action(f: () => Product) extends Product
-  case class Terminal(value: String) extends Product
+  trait Symbol
+  type Production = Seq[Symbol]
+  case class Nonterminal(name: String) extends Symbol
+  case class Terminal(value: String) extends Symbol
+  case class Action(f: () => Production) extends Symbol
 
-  type LeftSide = Symbol
-  type RightSide = Seq[Product]
+  type LeftSide = Nonterminal
+  type RightSide = Set[Production]
 
   type Grammar = Map[LeftSide, RightSide]
 
-  def start(grammar: Grammar): Option[RightSide] =
-    grammar.find({
-      case (key, _) => key.name == "start"
-    }).map({
-      case (_, product) => product
-    })
+  def takeRandom[T](xs: Traversable[T]): Option[T] =
+    util.Random.shuffle(xs).headOption
+
+  def produceRandom(grammar: Grammar, left: LeftSide): Option[Production] =
+    grammar.get(left).flatMap(productions => takeRandom(productions))
+
+  def reduceProduction(grammar: Grammar, production: Production): Seq[String] =
+    (for (symbol <- production) yield reduceSymbol(grammar, symbol)).flatten
+
+  def reduceSymbol(grammar: Grammar, symbol: Symbol): Seq[String] =
+    reduceSymbol(Seq.empty[String], grammar, symbol)
+
+  def reduceSymbol(result: Seq[String], grammar: Grammar, symbol: Symbol):
+      Seq[String] =
+    symbol match {
+      case Terminal(value) => result :+ value
+      case Nonterminal(name) =>
+        produceRandom(grammar, Nonterminal(name)) match {
+          case None => Seq.empty[String]
+          case Some(production) => reduceProduction(grammar, production)
+        }
+      case Action(f) => reduceProduction(grammar, f())
+    }
+
+  def start(grammar: Grammar): Option[Production] =
+    produceRandom(grammar, Nonterminal("start"))
+
+  def randomText(grammar: Grammar): Option[String] = {
+    produceRandom(grammar, Nonterminal("start")).map(production =>
+      reduceProduction(grammar, production).mkString(" ")
+    )
+  }
 }
