c3178f45985ebe5f4b11e1429480266d6e4592fb
William O'Hanley <liam@wohanley.com>
2015-02-09 18:33:32 +1300

    actions work :3

--- src/main/scala/grammars/asshole.scala
+++ src/main/scala/grammars/asshole.scala
@@ -11,17 +11,33 @@ object Asshole {
   def tms(values: String*): Set[Seq[Symbol]] = values.map(oneTm).toSet
 
   val grammar: Map[LeftSide, RightSide] = Map(
-    nt("start") -> Set(
-      oneNt("investigation"),
-      Seq(nt("greeting"), tm(", "), nt("investigation"))),
+    nt("start") -> Set(Seq(nt("greeting"), tm(", "), nt("body"))),
     nt("greeting") -> Set(
       oneNt("attention"),
       Seq(nt("attention"), tm(" "), nt("title"))),
     nt("attention") -> tms("hey", "sup", "yo", "hi", "what up"),
     nt("title") -> tms("bro", "dude", "man", "girl", "my man", "little lady"),
+    nt("body") -> Set(
+      Seq(
+        nt("investigation"), tm(" "),
+        nt("criticism"), tm(" "),
+        nt("advice")
+      ),
+      oneNt("investigation"),
+        Seq(
+          nt("investigation"), tm(" "),
+          nt("criticism")
+        ),
+      Seq(
+        nt("investigation"), tm(" "),
+        nt("advice")
+      )),
     nt("investigation") -> Set(
-      oneTm("that's sick"))
+      Seq(tm("looks like a "), Action(getSubject))
+    )
   )
+
+  def getSubject(): Production = oneTm("ham sandwich")
 
   def randomText(): Option[String] = grammars.randomText(grammar)
 }
