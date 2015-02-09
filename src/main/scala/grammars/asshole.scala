869d575bd25b0ea3777385b942d1a7a6c82fc6fa
William O'Hanley <liam@wohanley.com>
2015-02-09 17:15:33 +1300

    these will be handy

--- src/main/scala/grammars/asshole.scala
+++ src/main/scala/grammars/asshole.scala
@@ -5,8 +5,12 @@ import scala.collection.immutable.HashMap
 
 object Asshole {
 
+  // typing conveniences
+  def nt(name: String) = Nonterminal(name)
+  def tm(value: String) = Terminal(value)
+
   val grammar: Map[LeftSide, RightSide] = Map(
-    Nonterminal("start") -> Set(Seq(Terminal("Done.")))
+    nt("start") -> Set(Seq(tm("Done.")))
   )
 
   def randomText(): Option[String] = grammars.randomText(grammar)
