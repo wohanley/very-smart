d04ba95f78d7a07123c9877ffc6ec8c9808cfb51
William O'Hanley <liam@wohanley.com>
2015-02-10 16:23:55 +1300

    regenerated credentials and added some error handling

--- src/main/scala/web/package.scala
+++ src/main/scala/web/package.scala
@@ -1,17 +1,29 @@
 package object web {
 
+  import scala.util.Failure
+  import scala.util.Success
+  import scala.util.Try
   import util.Properties
   import twitter4j._
 
   def tweet(text: String) {
+
     val twitterConfig = new twitter4j.conf.ConfigurationBuilder()
-      .setOAuthConsumerKey("M4EOFJPkK08hpMoeOMLmKtIaE")
+      .setOAuthConsumerKey("rZL1YxeYrA9doVtp0wGO5ZK8W")
       .setOAuthConsumerSecret(Properties.envOrElse("API_SECRET", ""))
-      .setOAuthAccessToken("3015960198-3RKVdaRbPwwkVg1UGLQuO0PLysdta4aT1iSK1fe")
+      .setOAuthAccessToken("3015960198-jnI4BYRm0UPoYeMGtwM7Jjtu20cvn7rRXcxIylP")
       .setOAuthAccessTokenSecret(Properties.envOrElse("ACCESS_TOKEN_SECRET", ""))
       .build()
 
     val twitter = new TwitterFactory(twitterConfig).getInstance()
-    twitter.updateStatus(text.take(140))
+    val tweet = text.take(140)
+    Try(twitter.updateStatus(tweet)) match {
+      case Success(_) => Unit
+      case Failure(error) => {
+        println("Failed to tweet: " + tweet)
+        println("Tried with config: " + twitterConfig)
+        println("Error reported: " + error)
+      }
+    }
   }
 }
