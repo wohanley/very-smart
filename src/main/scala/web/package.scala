57c4b3efdd06115bbb46d99d12b6d5221750d26d
William O'Hanley <liam@wohanley.com>
2015-02-10 15:35:54 +1300

    moon prism power heroku

--- src/main/scala/web/package.scala
+++ src/main/scala/web/package.scala
@@ -5,9 +5,9 @@ package object web {
 
   def tweet(text: String) {
     val twitterConfig = new twitter4j.conf.ConfigurationBuilder()
-      .setOAuthConsumerKey("")
+      .setOAuthConsumerKey("M4EOFJPkK08hpMoeOMLmKtIaE")
       .setOAuthConsumerSecret(Properties.envOrElse("API_SECRET", ""))
-      .setOAuthAccessToken("")
+      .setOAuthAccessToken("3015960198-3RKVdaRbPwwkVg1UGLQuO0PLysdta4aT1iSK1fe")
       .setOAuthAccessTokenSecret(Properties.envOrElse("ACCESS_TOKEN_SECRET", ""))
       .build()
 
